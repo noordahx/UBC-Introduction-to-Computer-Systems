83edc25f07c321116d3156b9feab79209b6c8858
Noordahx <taumergenov@protonmail.com>
2022-10-29 18:00:41 -0700

    add: A6 assignment java code

--- SM213/src/arch/sm213/machine/student/CPU.java
+++ SM213/src/arch/sm213/machine/student/CPU.java
@@ -1,3 +1,7 @@
+// This code contains the solution to CPSC 213 Assignment 2.
+// Do not distribute this file or any portion of it to anyone.
+// Do not remove this comment.
+
 package arch.sm213.machine.student;
 
 import arch.sm213.machine.AbstractSM213CPU;
@@ -48,7 +52,7 @@ public class CPU extends AbstractSM213CPU {
     switch (opCode) {
       case 0x0:
       case 0xb:
-	long opExt = mem.readIntegerUnaligned (pcVal);
+        long opExt = mem.readIntegerUnaligned (pcVal) & 0xffffffffL;
 	pcVal += 4;
 	insOpExt.set    (opExt);
 	instruction.set (ins[0].value() << 40 | ins[1].value() << 32 | opExt);
@@ -79,110 +83,126 @@ public class CPU extends AbstractSM213CPU {
   @Override protected void execute () throws InvalidInstructionException, MachineHaltException, RegisterSet.InvalidRegisterNumberException, MainMemory.InvalidAddressException
   {
     switch (insOpCode.get()) {
-      case 0x0: // ld $i, d .............. 0d-- iiii iiii
+
+      case 0x0: // ld $v, d .............. 0d-- vvvv vvvv
         reg.set (insOp0.get(), insOpExt.get());
         break;
+
       case 0x1: // ld o(rs), rd .......... 1psd  (p = o / 4)
-        // TODO
-        int address = reg.get(insOp1.get());
-        address += insOp0.get()*4;
-        int value = mem.readInteger(address);
-        reg.set (insOp2.get(), value);
+        reg.set (insOp2.get(), mem.readInteger ((insOp0.get() << 2) + reg.get (insOp1.get())));
         break;
-      case 0x2: // ld (rs, ri, 4), rd .... 2sid
-        // TODO
-        address = reg.get(insOp0.get());
-        address += reg.get(insOp1.get()) * 4;
-        value = mem.readInteger(address);
-        reg.set(insOp2.get(), value);
+
+      case 0x2: // ld (rs, ri, 2), rd .... 2sid
+        reg.set (insOp2.get(), mem.readInteger (reg.get (insOp0.get()) + (reg.get (insOp1.get())<<2)));
         break;
+
       case 0x3: // st rs, o(rd) .......... 3spd  (p = o / 4)
-        // TODO
-        address = reg.get(insOp2.get());
-        address += insOp1.get()*4;
-        value = reg.get(insOp0.get());
-        mem.writeInteger(address, value);
+        mem.writeInteger ((insOp1.get() << 2) + reg.get (insOp2.get()), reg.get (insOp0.get()));
         break;
+
       case 0x4: // st rs, (rd, ri, 4) .... 4sdi
-        // TODO
-        address = reg.get(insOp1.get());
-        address += reg.get(insOp2.get()) * 4;
-        value = reg.get(insOp0.get());
-        mem.writeInteger(address, value);
+        mem.writeInteger (reg.get (insOp1.get()) + (reg.get (insOp2.get())<<2), reg.get (insOp0.get()));
         break;
+
       case 0x6: // ALU ................... 6-sd
-	switch (insOp0.get()) {
-	  case 0x0: // mov rs, rd ........ 60sd
+        switch (insOp0.get()) {
+
+          case 0x0: // mov rs, rd ........ 60sd
+            reg.set (insOp2.get(), reg.get (insOp1.get()));
+            break;
+
+          case 0x1: // add rs, rd ........ 61sd
+            reg.set (insOp2.get(), reg.get (insOp1.get()) + reg.get (insOp2.get()));
+            break;
+
+          case 0x2: // and rs, rd ........ 62sd
+            reg.set (insOp2.get(), reg.get (insOp1.get()) & reg.get (insOp2.get()));
+            break;
+
+          case 0x3: // inc rr ............ 63-r
+            reg.set (insOp2.get(), reg.get (insOp2.get()) + 1);
+            break;
+
+          case 0x4: // inca rr ........... 64-r
+            reg.set (insOp2.get(), reg.get (insOp2.get()) + 4);
+            break;
+
+          case 0x5: // dec rr ............ 65-r
+            reg.set (insOp2.get(), reg.get (insOp2.get()) - 1);
+            break;
+
+          case 0x6: // deca rr ........... 66-r
+            reg.set (insOp2.get(), reg.get (insOp2.get()) - 4);
+            break;
+
+          case 0x7: // not ............... 67-r
+            reg.set (insOp2.get(), ~reg.get (insOp2.get()));
+            break;
+
+          case 0xf: // gpc ............... 6fpr
             // TODO
-        value = reg.get(insOp1.get());
-        reg.set(insOp2.get(), value);
-	    break;
-	  case 0x1: // add rs, rd ........ 61sd
-            // TODO
-        value = reg.get(insOp1.get());
-        value += reg.get(insOp2.get());
-        reg.set(insOp2.get(), value);
-	    break;
-	  case 0x2: // and rs, rd ........ 62sd
-            // TODO
-        value = reg.get(insOp1.get());
-        value &= reg.get(insOp2.get());
-        reg.set(insOp2.get(), value);
-	    break;
-	  case 0x3: // inc rr ............ 63-r
-            // TODO
-        value = reg.get(insOp2.get());
-        value++;
-        reg.set(insOp2.get(), value);
-	    break;
-	  case 0x4: // inca rr ........... 64-r
-            // TODO
-        value = reg.get(insOp2.get());
-        value+=4;
-        reg.set(insOp2.get(), value);
-	    break;
-	  case 0x5: // dec rr ............ 65-r
-            // TODO
-        value = reg.get(insOp2.get());
-        value--;
-        reg.set(insOp2.get(), value);
-	    break;
-	  case 0x6: // deca rr ........... 66-r
-            // TODO
-        value = reg.get(insOp2.get());
-        value-=4;
-        reg.set(insOp2.get(), value);
-	    break;
-	  case 0x7: // not ............... 67-r
-            // TODO
-        value = reg.get(insOp2.get());
-        value = ~value;
-        reg.set(insOp2.get(), value);
-	    break;
-	  default:
-	    throw new InvalidInstructionException();
-	}
-	break;
-      case 0x7: // sh? $i,rd ............. 7dii
-        // TODO
-        value = reg.get(insOp0.get());
-        byte firstHex = (byte) (insOp1.get() << 4);
-        byte secondHex = (byte) (insOp2.get() & 0xf);
-        byte ss = (byte) (firstHex | secondHex);
-        if (ss > 0){
-            reg.set(insOp0.get(), (value << ss));
-        } else {
-            reg.set(insOp0.get(), (value >> (-ss)));
+            reg.set (insOp2.get(), pc.get() + insOp1.get() * 2);
+            break;
+
+          default:
+            throw new InvalidInstructionException();
         }
         break;
-      case 0xf: // halt or nop ............. f?--
-	if (insOp0.get() == 0)
-	  // halt .......................... f0--
-	  throw new MachineHaltException();
-	else if (insOp0.getUnsigned() == 0xf)
-	  // nop ........................... ff--
-	  break;
+
+      case 0x7: // sh? $i,rd ............. 7dii
+        if (insOpImm.get() > 0)
+          reg.set (insOp0.get(), (long) reg.get(insOp0.get()) << insOpImm.get());
+        else
+          reg.set (insOp0.get(), reg.get (insOp0.get()) >> -insOpImm.get());
         break;
+
+      case 0x8: // br a .................. 8-pp  (a = pc + pp * 2)
+        // TODO
+        int pcVal = pc.get();
+        int jumpVal = (insOp1.get() * 16 + insOp2.getUnsigned()) * 2;
+        pc.set(pcVal + jumpVal);
+        break;
+
+      case 0x9: // beq rs, a ............. 9rpp  (a = pc + pp * 2)
+        // TODO
+        if (reg.get(insOp0.get()) == 0) {
+            pcVal = pc.get();
+            jumpVal = (insOp1.get() * 16 + insOp2.getUnsigned()) * 2;
+            pc.set(pcVal + jumpVal);
+        }
+        break;
+
+      case 0xa: // bg rs, a .............. arpp  (a = pc + pp * 2)
+        // TODO
+        if (reg.get(insOp0.get()) > 0) {
+            pcVal = pc.get();
+            jumpVal = (insOp1.get() * 16 + insOp2.getUnsigned()) * 2;
+            pc.set(pcVal + jumpVal);
+        }
+        break;
+
+      case 0xb: // j i ................... b--- iiii iiii
+        // TODO
+        jumpVal = insOpExt.get();
+        pc.set(jumpVal);
+        break;
+
+      case 0xc: // j o(rr) ............... crpp  (pp = o / 2)
+        // TODO
+        pcVal = pc.get();
+        jumpVal = reg.get(insOp0.get()) + 2 * (insOp1.getUnsigned() * 16 + insOp2.getUnsigned());
+        pc.set(jumpVal);
+        break;
+
+      case 0xf: // halt or nop ............. f?--
+        if (insOp0.get() == 0)
+          // halt .......................... f0--
+          throw new MachineHaltException();
+        else if (insOp0.getUnsigned() == 0xf)
+          // nop ........................... ff--
+          break;
+        break;
+        
       default:
         throw new InvalidInstructionException();
     }
