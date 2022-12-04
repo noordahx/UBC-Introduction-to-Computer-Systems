1a6264f0b8068f9b5be47d63a19b62ca95481633
Noordahx <taumergenov@protonmail.com>
2022-12-03 20:42:21 -0800

    fix: q1 solution (removed mutex, managing threads using only uthread_join)

--- A10/a10/q1.c
+++ A10/a10/q1.c
@@ -1,63 +1,41 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "uthread.h"
-#include "uthread_mutex_cond.h"
 
 uthread_t t0, t1, t2;
-uthread_mutex_t mx;
-uthread_cond_t aCond, aCond2;
-int lock = 0, lock2 = 0;
+
 void randomStall() {
-  int i, r = random() >> 16;
-  while (i++<r);
+  //int i, r = random() >> 16;
+  //while (i++<r);
 }
 
 void* p0(void* v) {
   randomStall();
-  uthread_mutex_lock(mx);
-    printf("zero\n");
-    lock = 1;
-    uthread_cond_signal(aCond);  
-  uthread_mutex_unlock(mx);
+  printf("zero\n");
   return NULL;
 }
 
 void* p1(void* v) {
   randomStall();
-  uthread_mutex_lock(mx);
-    while(!lock) 
-      uthread_cond_wait(aCond);
-    printf("one\n");
-    lock = 0;
-    lock2 = 1;
-    uthread_cond_signal(aCond2);
-  uthread_mutex_unlock(mx);
+  printf("one\n");
   return NULL;
 }
 
 void* p2(void* v) {
   randomStall();
-  uthread_mutex_lock(mx);
-    while(!lock2) 
-      uthread_cond_wait(aCond2);
-    printf("two\n");
-    lock2 = 0;
-  uthread_mutex_unlock(mx);
+  printf("two\n");
   return NULL;
   }
 
 int main(int arg, char** arv) {
   uthread_init(4);
-  mx = uthread_mutex_create();
-  aCond = uthread_cond_create(mx);
-  aCond2 = uthread_cond_create(mx);
   t0 = uthread_create(p0, NULL);
-  t1 = uthread_create(p1, NULL);
-  t2 = uthread_create(p2, NULL);
-  randomStall();
   uthread_join(t0, NULL);
+  t1 = uthread_create(p1, NULL);
   uthread_join(t1, NULL);
+  t2 = uthread_create(p2, NULL);
   uthread_join(t2, NULL);
+  randomStall();
   printf("three\n");
   printf("------\n");
 }
