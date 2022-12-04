7b0c23320cb0251c4a6cfdbaa50b1c07def08a18
Noordahx <taumergenov@protonmail.com>
2022-12-03 20:22:40 -0800

    add: q2 solution

--- A10/a10/q2.c
+++ A10/a10/q2.c
@@ -7,15 +7,27 @@ const int N = 100000;
 int cnt = 0;
 
 uthread_t t0, t1, t2;
+uthread_mutex_t mx;
+uthread_cond_t aCond;
+int lock = 0;
 
 void countUp() {
-  for (int i=0; i<N; i++)
-    cnt += 1;
+  uthread_mutex_lock(mx);
+    lock = 1;
+    for (int i=0; i<N; i++)
+      cnt += 1;
+    uthread_cond_signal(aCond);
+  uthread_mutex_unlock(mx);
 }
 
 void countDown() {
-  for (int i=0; i<N; i++)
-    cnt -= 1;
+  uthread_mutex_lock(mx);
+    while(!lock)
+      uthread_cond_wait(aCond);
+    for (int i=0; i<N; i++)
+      cnt -= 1;
+    lock = 0;
+  uthread_mutex_unlock(mx);
 }
 
 void* count(void* v) {
@@ -26,6 +38,9 @@ void* count(void* v) {
 
 int main(int arg, char** arv) {
   uthread_init(4);
+  mx = uthread_mutex_create();
+  aCond = uthread_cond_create(mx);
+
   t0 = uthread_create(count, NULL);
   t1 = uthread_create(count, NULL);
   t2 = uthread_create(count, NULL);
