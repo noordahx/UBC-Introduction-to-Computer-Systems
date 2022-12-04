f5c1d5df728477e7222bdac41e3b3abd8d6312ae
Noordahx <taumergenov@protonmail.com>
2022-12-03 20:38:53 -0800

    add: q3 solution

--- A10/a10/q3.c
+++ A10/a10/q3.c
@@ -5,6 +5,9 @@
 
 #define NUM_THREADS 3
 uthread_t threads[NUM_THREADS];
+uthread_mutex_t mx;
+uthread_cond_t aCond;
+int lock = 0;
 
 void randomStall() {
   int i, r = random() >> 16;
@@ -12,18 +15,35 @@ void randomStall() {
 }
 
 void waitForAllOtherThreads() {
+  uthread_mutex_lock(mx);
+    while(lock != NUM_THREADS) {
+      uthread_cond_wait(aCond);
+    }
+  uthread_mutex_unlock(mx);
 }
 
 void* p(void* v) {
   randomStall();
-  printf("a\n");
+  
+  uthread_mutex_lock(mx);
+    lock++;
+    printf("a\n");
+    uthread_cond_broadcast(aCond);
+  uthread_mutex_unlock(mx);
+
   waitForAllOtherThreads();
+
   printf("b\n");
   return NULL;
 }
 
 int main(int arg, char** arv) {
   uthread_init(4);
+  
+  mx = uthread_mutex_create();
+  aCond = uthread_cond_create(mx);
+  lock = 0;
+  
   for (int i=0; i<NUM_THREADS; i++)
     threads[i] = uthread_create(p, NULL);
   for (int i=0; i<NUM_THREADS; i++)
